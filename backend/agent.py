94e87da05ce47f0658ff7312d5a7d9d1fb49d79d
Akshat <cruise.akshat@gmail.com>
2025-11-22 17:56:12 +0530

    refactor: updated agent functions

--- backend/agent.py
+++ backend/agent.py
@@ -1,56 +1,28 @@
-import os
-from langchain.agents import Tool, initialize_agent
-from langchain.chat_models import ChatOpenAI
-from langchain.agents import AgentType
-from actions import get_weather, get_time, calculate, summarize
-from openai import OpenAI
+from config import OPENAI_API_KEY
+from actions import get_time, get_weather, calculate, summarize
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
 
 
-"""OPENAI Client Initialization"""
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-
-"""Sample Tools for Agent"""
-tools = [
-    Tool(
-        name="get_weather",
-        func=get_weather,
-        description="Use this to get weather information for a given city. Input should be the city name."
-    ),
-    Tool(
-        name="get_time",
-        func=get_time,
-        description="Use this to get current time in a city. Input should be the city name."
-    ),
-    Tool(
-        name="calculate",
-        func=calculate,
-        description="Use this for evaluating math expressions. Input should be a math expression string."
-    ),
-    Tool(
-        name="summarize_text",
-        func=summarize,
-        description="Summarize the given text."
-    ),
-
-]
-
-
-"""LLM and Agent Initialization"""
 llm = ChatOpenAI(
+    model="gpt-4o-mini",
     temperature=0,
-    model="gpt-4o-mini"  # Small, fast, cheap for your project
+    api_key=OPENAI_API_KEY
 )
 
 
-agent = initialize_agent(
-    tools,
-    llm,
-    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True
-)
+tools = [get_time, get_weather, calculate, summarize]
+
+app = create_react_agent(llm, tools)
 
 
-def run_agent(query: str):
-    """Function to run the agent with given query."""
-    return agent.run(query)
+async def run_agent(user_input: str):
+
+    result = app.invoke({
+        "messages": [
+            {"role": "user", "content": user_input}
+        ]
+    })
+
+    final = result["messages"][-1].content
+    return final
